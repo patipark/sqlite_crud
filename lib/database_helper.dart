0fd5e5feaf56dfa294b531a0918fa19a0ab99c33
Wad-Laptop <iamwad@gmail.com>
2024-08-16 18:59:44 +0700

    create database helper

---
+++ lib/database_helper.dart
@@ -0,0 +1,86 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('todos.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE todos(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        description TEXT
+      )
+    ''');
+  }
+
+  Future<int> createTodo(Todo todo) async {
+    final db = await instance.database;
+    return await db.insert('todos', todo.toMap());
+  }
+
+  Future<List<Todo>> readAllTodos() async {
+    final db = await instance.database;
+    final result = await db.query('todos');
+    return result.map((json) => Todo.fromMap(json)).toList();
+  }
+
+  Future<int> updateTodo(Todo todo) async {
+    final db = await instance.database;
+    return await db.update(
+      'todos',
+      todo.toMap(),
+      where: 'id = ?',
+      whereArgs: [todo.id],
+    );
+  }
+
+  Future<int> deleteTodo(int id) async {
+    final db = await instance.database;
+    return await db.delete(
+      'todos',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
+
+class Todo {
+  final int? id;
+  final String title;
+  final String description;
+
+  Todo({this.id, required this.title, required this.description});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+    };
+  }
+
+  static Todo fromMap(Map<String, dynamic> map) {
+    return Todo(
+      id: map['id'],
+      title: map['title'],
+      description: map['description'],
+    );
+  }
+}
